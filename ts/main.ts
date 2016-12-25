82b1fb7557736929d10c16174b3f265c36e20189
Colwyn Fritze-Moor <colwyn.myself@gmail.com>
2016-12-25 12:18:21 -0800

    Removed koa-body and added koa-better-body. Added global error handler

--- ts/main.ts
+++ ts/main.ts
@@ -6,15 +6,27 @@ const chalk = require('chalk');
 // Module constructor imports
 const Debug = require('debug');
 const Logger = require('koa-logger');
-const KoaBody = require('koa-body');
+const KoaBody = require('koa-better-body');
 
 // Classes
 import { Response } from './classes/Response';
 
 // Object inits
 const debug = Debug('colwyn:main');
-const app = koa();
 const logger = Logger();
+
+// App initialization
+const app = koa();
+// Global error handling
+app.use(function* globalError(next) {
+    try {
+        yield next;
+    } catch (systemErr) {
+        debug(systemErr);
+        this.status = 500;
+        this.body = 'Internal server error.';
+    }
+});
 
 // Server variables
 const port = 3000;
@@ -26,7 +38,7 @@ app.use(KoaBody());
 
 // Routes
 app.use(route.post('/terminal-input', function* terminalnput() {
-    const data = this.request.body;
+    const data = this.request.fields;
     const input = data.input;
     const responseObject = new Response(input);
     const output = responseObject.response;
