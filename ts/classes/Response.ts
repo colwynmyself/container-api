c1b9161fe031cf52ddc84852bf1ee37caf3a8806
Colwyn Fritze-Moor <colwyn.myself@gmail.com>
2016-12-29 17:33:34 -0800

    Added custom error handler for debugging, improved tsc

--- ts/classes/Response.ts
+++ ts/classes/Response.ts
@@ -1,23 +1,15 @@
 import { responseHash } from '../data';
 
-class Response {
-    _input: String;
-    constructor(input) {
-        if (typeof input !== 'string') throw new Error(`Response class must be initialized with string input.\
-            ${input} is an invalid type: ${typeof input}`);
-
-        this._input = input.trim();
+export class Response {
+    public readonly input: string;
+    constructor(input: string) {
+        if (!input) throw new Error('class Response must be initialized with a string.');
+        this.input = input.trim();
     }
 
-    get input() { return this._input; }
-    get response() { return this.responseLookup(this._input); }
+    get response() { return this.responseLookup(this.input); }
 
-    responseLookup(input) {
-        if (!input) return;
+    protected responseLookup(input: string) {
         return responseHash[input] || `${input}: command not found`;
     }
 }
-
-export {
-    Response,
-};
--- ts/main.ts
+++ ts/main.ts
@@ -1,15 +1,16 @@
 // Module imports
-const koa = require('koa');
-const route = require('koa-route');
-const chalk = require('chalk');
+import * as koa from 'koa';
+import * as route from 'koa-route';
+import * as chalk from 'chalk';
 
 // Module constructor imports
-const Debug = require('debug');
-const Logger = require('koa-logger');
-const KoaBody = require('koa-better-body');
+import * as Debug from 'debug';
+import * as Logger from 'koa-logger';
+import * as KoaBody from 'koa-better-body';
 
 // Classes
 import { Response } from './classes/Response';
+import { CustomError } from './classes/CustomError';
 
 // Object inits
 const debug = Debug('colwyn:main');
@@ -18,13 +19,14 @@ const logger = Logger();
 // App initialization
 const app = koa();
 // Global error handling
-app.use(function* globalError(next) {
+app.use(function* globalError(next: any) {
     try {
         yield next;
-    } catch (systemErr) {
-        debug(systemErr);
+    } catch (err) {
+        const customErr = new CustomError(err.message);
+        customErr.debugError(this.request);
         this.status = 500;
-        this.body = 'Internal server error.';
+        this.body = 'Internal server error: ' + err.message;
     }
 });
 
@@ -40,6 +42,8 @@ app.use(KoaBody());
 app.use(route.post('/terminal-input', function* terminalnput() {
     const data = this.request.fields;
     const input = data.input;
+    // This is pretty lazy right now, the global error handler doesn't do a good job of responding with what the actual
+    // problem that occured is.
     const responseObject = new Response(input);
     const output = responseObject.response;
     this.body = { input, output };
